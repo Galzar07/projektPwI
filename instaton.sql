44489d91938a0db72ee717810027f6ddd739c4cf
Galzar07 <52508198+Galzar07@users.noreply.github.com>
2020-03-24 20:25:06 +0100

    three imit

---
+++ instaton.sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 24 Mar 2020, 20:22
+-- Wersja serwera: 10.4.11-MariaDB
+-- Wersja PHP: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `instaton`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `komentarze`
+--
+
+CREATE TABLE `komentarze` (
+  `id_kom` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `tresc` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `konto`
+--
+
+CREATE TABLE `konto` (
+  `id_konto` int(11) NOT NULL,
+  `polubienia` int(11) NOT NULL,
+  `data_zalozenia` date NOT NULL,
+  `id_obserwujacy` int(11) NOT NULL,
+  `id_obserwatorzy` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `obserwatorzy`
+--
+
+CREATE TABLE `obserwatorzy` (
+  `id_obserwatorzy` int(11) NOT NULL,
+  `nazwa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `obserwujacy`
+--
+
+CREATE TABLE `obserwujacy` (
+  `id_obserwujacy` int(11) NOT NULL,
+  `nazwa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` char(64) NOT NULL,
+  `id_konto` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `komentarze`
+--
+ALTER TABLE `komentarze`
+  ADD PRIMARY KEY (`id_kom`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeksy dla tabeli `konto`
+--
+ALTER TABLE `konto`
+  ADD PRIMARY KEY (`id_konto`),
+  ADD KEY `id_obserwatorzy` (`id_obserwatorzy`),
+  ADD KEY `id_obserwujacy` (`id_obserwujacy`);
+
+--
+-- Indeksy dla tabeli `obserwatorzy`
+--
+ALTER TABLE `obserwatorzy`
+  ADD PRIMARY KEY (`id_obserwatorzy`);
+
+--
+-- Indeksy dla tabeli `obserwujacy`
+--
+ALTER TABLE `obserwujacy`
+  ADD PRIMARY KEY (`id_obserwujacy`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `id_konto` (`id_konto`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `komentarze`
+--
+ALTER TABLE `komentarze`
+  MODIFY `id_kom` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `konto`
+--
+ALTER TABLE `konto`
+  MODIFY `id_konto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `obserwatorzy`
+--
+ALTER TABLE `obserwatorzy`
+  MODIFY `id_obserwatorzy` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `obserwujacy`
+--
+ALTER TABLE `obserwujacy`
+  MODIFY `id_obserwujacy` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `komentarze`
+--
+ALTER TABLE `komentarze`
+  ADD CONSTRAINT `komentarze_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`);
+
+--
+-- Ograniczenia dla tabeli `konto`
+--
+ALTER TABLE `konto`
+  ADD CONSTRAINT `konto_ibfk_1` FOREIGN KEY (`id_obserwatorzy`) REFERENCES `obserwatorzy` (`id_obserwatorzy`),
+  ADD CONSTRAINT `konto_ibfk_2` FOREIGN KEY (`id_obserwujacy`) REFERENCES `obserwujacy` (`id_obserwujacy`);
+
+--
+-- Ograniczenia dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`id_konto`) REFERENCES `konto` (`id_konto`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
